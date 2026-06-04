94bedbd5538829386c7dbf52868ac3438bc60329
arch-antomcore <matheus.peres@mail.fae.edu>
2026-06-04 13:25:21 -0300

    fix: adjust paths for GitHub Pages deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import ProductDetail from "@/pages/ProductDetail";
 function App() {
   return (
     <div className="App grain">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/produto/:id" element={<ProductDetail />} />
